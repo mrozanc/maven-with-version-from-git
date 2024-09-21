cb8f46a652075a65e9bd934c7faf1959e60eafe4
Marc ROZANC <marc.git.dev@mro.slmail.me>
2024-09-21 13:06:38 +0200

    Add some dependencies

--- src/main/java/fr/rozanc/sandbox/Main.java
+++ src/main/java/fr/rozanc/sandbox/Main.java
@@ -1,11 +1,28 @@
 package fr.rozanc.sandbox;
 
+import java.io.PrintStream;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        new Main().run(args);
+    }
+
+    private final PrintStream out;
+
+    Main(PrintStream out) {
+        this.out = out;
+    }
+
+    Main() {
+        this(System.out);
+    }
+
+    void run(String[] args) {
+        out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            out.println("i = " + i);
         }
     }
 }
---
+++ src/test/java/fr/rozanc/sandbox/MainTest.java
@@ -0,0 +1,23 @@
+package fr.rozanc.sandbox;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MainTest {
+
+    @Test
+    void should_print_hello() throws IOException {
+        try (final ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            final PrintStream out = new PrintStream(outputStream);
+
+            new Main(out).run(new String[0]);
+
+            assertThat(outputStream.toString()).startsWith("Hello and welcome!" + System.lineSeparator());
+        }
+    }
+}
